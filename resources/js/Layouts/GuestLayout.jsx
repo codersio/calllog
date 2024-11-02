0f2c671291a24213bdfdd24be3f6622e249a811a
CosmicLegend77 <110524634+CosmicLegend77@users.noreply.github.com>
2024-11-02 17:35:28 +0530

    0535021124

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -5,7 +5,7 @@ export default function Guest({ children }) {
     return (
         <div className="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0 ">
             <div className='grid place-items-center'>
-                 <img src="/SCS-01-removebg-preview.png" alt="Description" className="w-[80%]" />
+                 <img src="/logo/logo2.jpg" alt="Description" className="w-[80%]" />
             </div>
 
             <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md ">
